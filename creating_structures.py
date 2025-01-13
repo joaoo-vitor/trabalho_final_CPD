c845c005726f7522ceb2b1dea3494da3ad519869
joaoo-vitor <joao.vitor@meta.com.br>
2025-01-13 01:38:16 -0300

    time tests with pandas and csv library

--- creating_structures.py
+++ creating_structures.py
@@ -2,6 +2,7 @@ import pandas as pd
 from ternary_trie import *
 from hashing import *
 from trie import *
+import datetime
 
 df_players = pd.read_csv('players.csv')
 df_ratings = pd.read_csv('minirating.csv')
@@ -10,6 +11,7 @@ df_tags = pd.read_csv('tags.csv')
 print('Criando estruturas...')
 
 # 2.1 Estrutura 1: Armazenando Dados Sobre Jogadores
+before = datetime.datetime.now()
 # Define colunas novas em df_players para os ratings
 df_players['number_of_ratings']=0
 df_players['rating_avg']=0
@@ -40,13 +42,19 @@ for key in hash_table_players:
 
 # player = search_hash_table(158023, 'sofifa_id', hash_table_players, ht_size)
 # print(player['rating_avg'])
+elapsed_time = datetime.datetime.now() - before
+print('Estrutura 1 finalizada. Tempo: ', elapsed_time)
 
 # 2.2 Estrutura 2: Estrutura para buscas por strings de nomes
+before = datetime.datetime.now()
 trie_names = TernaryTrie()
 for _, row in df_players.iterrows():
    trie_names.insert(row['long_name'].lower(), row['sofifa_id'])
+elapsed_time = datetime.datetime.now() - before
+print('Estrutura 2 finalizada. Tempo: ', elapsed_time)
 
 # 2.3 Estrutura 3: Estrutura para guardar revisoes de usuários
+before = datetime.datetime.now()
 hash_table_users = start_hash_table(ht_size)
 
 # Insere cada rating na lista da posição adequada 
@@ -63,15 +71,18 @@ for index, row in df_ratings.iterrows():
        dict_ratings['user_id']=row['user_id']
        dict_ratings['ratings'] = [(player_id, rating)]
        insert_hash_table(key, dict_ratings, hash_table_users, ht_size)
+       
+elapsed_time = datetime.datetime.now() - before
+print('Estrutura 3 finalizada. Tempo: ', elapsed_time)
 
 # user_ratings = search_hash_table(12320, 'user_id', hash_table_users, ht_size)
 # print(user_ratings['ratings'])
 
-# 2.4 Estrutura 4: Estrutura para guardar tags
-trie_tags = Trie()
-for _, row in df_tags.iterrows():
-   trie_tags.insert(row['sofifa_id'], str(row['tag']))
+# # 2.4 Estrutura 4: Estrutura para guardar tags
+# trie_tags = Trie()
+# for _, row in df_tags.iterrows():
+#    trie_tags.insert(row['sofifa_id'], str(row['tag']))
 
-print(trie_tags.search('B'))
+# print(trie_tags.search('B'))
 
 
--- time_tests.py
+++ time_tests.py
@@ -1,13 +1,48 @@
 import pandas as pd
 from ternary_trie import *
 from hashing import *
+import datetime
+import csv
 
-df_ratings = pd.read_csv('rating.csv')
 
-# # 2.3 Estrutura 1: Armazenando Dados Sobre Jogadores
-ht_size=12323
-hash_table_ratings = start_hash_table(ht_size)
+before = datetime.datetime.now()
+df_tags = pd.read_csv('tags.csv')
+count1 = 0
+count2=0
+count3=0
 
-# Insere cada rating na lista da posição adequada 
-for index, row in df_ratings.iterrows():
-    _, user_ratings= search_hash_table(row['user_id'], 'user_id', hash_table_ratings, ht_size)
+for _, row in df_tags.iterrows():
+    if(row['tag']):
+        count1+=1
+time1 =  datetime.datetime.now() - before
+
+print('Pandas time: ', time1)
+
+
+before = datetime.datetime.now()
+with open('tags.csv', "r") as arquivo_tags:
+        arquivo_csv = csv.reader(arquivo_tags, delimiter=",")
+        next(arquivo_csv)
+        for linha in arquivo_csv:
+            if(linha[2]):
+                count2+=1
+              
+time2 = datetime.datetime.now() - before
+print('Csv time: ', time2)
+
+before = datetime.datetime.now()
+def process_chunk(chunk):
+    count3=0
+    # Perform operations on the chunk (e.g., filtering, aggregation)
+    for _, row in chunk.iterrows():
+        if(row['tag']):
+            count3+=1
+    return chunk, count3
+for chunk in pd.read_csv('tags.csv', chunksize=10000):
+    processed_chunk, count = process_chunk(chunk)
+    count3 +=count
+
+time3 = datetime.datetime.now() - before
+print('Pandas with chunksize: ', time3)
+print('Counts: ', count1, count2, count3)
+
