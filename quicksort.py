7e397fc68cd86b81af43f209bfded0e70a2df37e
joaoo-vitor <joao.vitor@meta.com.br>
2025-01-13 23:10:32 -0300

    found a bug on third structure, first two searches are ok

--- quicksort.py
+++ quicksort.py
@@ -1,62 +1,24 @@
 
-def quick_sort_lomuto(arr, i, f, trocas=0, recursoes=0):
-    recursoes +=1
+def quick_sort_lomuto(arr, key_idx, i, f, dec=False):
     if(f>i):
         # partition into two parts
-        p, trocas = partition_lomuto(arr, i, f, trocas)
+        p = partition_lomuto(arr, key_idx, i, f, dec)
         # make recursion for both parts 
-        trocas, recursoes = quick_sort_lomuto(arr, i, p-1, trocas, recursoes)
-        trocas, recursoes = quick_sort_lomuto(arr, p+1, f, trocas, recursoes)
-    return trocas, recursoes
+        quick_sort_lomuto(arr, key_idx, i, p-1, dec)
+        quick_sort_lomuto(arr, key_idx, p+1, f, dec)
 
-def quick_sort_hoare(arr, i, f, trocas=0, recursoes=0):
-    recursoes += 1
-    if(f>i):
-        # partition into two parts
-        p, trocas = partition_hoare(arr, i, f, trocas)
-        # make recursion for both parts 
-        trocas, recursoes = quick_sort_hoare(arr, i, p-1, trocas, recursoes)
-        trocas, recursoes = quick_sort_hoare(arr, p+1, f, trocas, recursoes)
-    return trocas, recursoes
-
-def partition_lomuto(arr, left, right, trocas):
-    chave = arr[left]
+def partition_lomuto(arr, key_idx, left, right, dec):
+    pivot = float(arr[left][key_idx])
     storeIndex = left+1 # index of smaller element
     for i in range(left+1, right+1):
         # if the element is smaller than or equal to the pivot
-        if(arr[i] < chave):
+        if (not dec and float(arr[i][key_idx]) < pivot) or (dec and float(arr[i][key_idx]) > pivot):
             #swap values
             swap(arr, i, storeIndex)
-            trocas+=1
             storeIndex+=1
     swap(arr, left, storeIndex-1)
-    trocas+=1
-    return storeIndex-1, trocas
+    return storeIndex-1
 
-def partition_hoare(arr, left, right, trocas):
-    chave = arr[left]
-    i = left
-    j = right + 1
-    
-    while True:
-        i += 1
-        while (arr[i] <= chave and i < right):
-            i += 1
-        
-        j -= 1
-        while (arr[j] >= chave and j > left):
-            j -= 1
-
-        if i >= j:
-            break        
-        swap(arr, i, j)
-        trocas += 1
-
-    swap(arr, left, j)
-    trocas += 1
-
-    return j, trocas
-    
 def swap(arr, a, b):
     tmp = arr[a]
     arr[a]=arr[b]
--- searches.py
+++ searches.py
@@ -3,18 +3,25 @@ from ternary_trie import *
 from hashing import *
 from trie import *
 from prettytable import PrettyTable
+from quicksort import quick_sort_lomuto
 
 # 3.1 Pesquisa 1: prefixos de nomes de jogadores
 def prefixo(prefix, names_trie, ht_players):  
-    ids_players = names_trie.search(prefix)
     result = PrettyTable()
     result.field_names = ['sofifa_id', 'short_name', 'long_name', 'player_positions',  'nationality', 'rating_avg', 'number_of_ratings']
-    for id in ids_players:
-        player = ht_players.search(id, 'sofifa_id')
-        player['rating_avg'] = '%0.6f' % player['rating_avg']  
-        result.add_row([player[column] for column in result.field_names])
-    # Ordena em ordem decrescente a partir do rating global (TODO)
-
+    ids_players = names_trie.search(prefix)
+    arr_rows = []
+    if(ids_players):
+        for id in ids_players:
+            player = ht_players.search(id, 'sofifa_id')
+            player['rating_avg'] = '%0.6f' % player['rating_avg']  
+            arr_rows.append([player[column] for column in result.field_names])
+            print('adding to the table: \n', [player[column] for column in result.field_names])
+        # Ordena em ordem decrescente a partir do rating global (index 5)
+        quick_sort_lomuto(arr_rows, 5, 0, len(arr_rows)-1, dec=True)
+    # Adiciona da array para a tabela
+    for row in arr_rows:
+        result.add_row(row)
     return result
 
 # 3.2 Pesquisa 2: jogadores revisados por usuários
@@ -22,10 +29,23 @@ def user(user_id, ht_users, ht_players):
     result = PrettyTable()
     result.field_names = ['sofifa_id', 'short_name', 'long_name', 'rating_avg', 'number_of_ratings', "user's rating"]
     ratings_user = ht_users.search(user_id, 'user_id')
-    for rating in ratings_user['ratings']:
-        player = ht_players.search(rating[0], 'sofifa_id')
-        player['rating_avg'] = '%0.6f' % player['rating_avg']  
-        result.add_row([player['sofifa_id'], player['short_name'], player['long_name'], player['rating_avg'], player['number_of_ratings'], rating[1]])
-    # Ordena em ordem decrescente a partir do rating global (TODO)
-    # Remove o sobrejacente (apenas 30 ) (TODO)
+    print(f'encontrei {len(ratings_user)} ratings desse usuario')
+    arr_rows = []
+    if(ratings_user):
+        for rating in ratings_user['ratings']:
+            player = ht_players.search(rating[0], 'sofifa_id')
+            player['rating_avg'] = '%0.6f' % player['rating_avg']  
+            arr_rows.append([player['sofifa_id'], player['short_name'], player['long_name'], player['rating_avg'], player['number_of_ratings'], rating[1]])
+        # Ordena em ordem decrescente a partir do rating global (index 3)
+        quick_sort_lomuto(arr_rows, 3, 0, len(arr_rows)-1, dec=True)
+        # Remove o sobrejacente (apenas 30)
+        count =0
+        for row in arr_rows:
+            result.add_row(row)
+            count+=1
+            if(count==30):
+                break
+    # Adiciona da array para a tabela
+    for row in arr_rows:
+        result.add_row(row)
     return result
